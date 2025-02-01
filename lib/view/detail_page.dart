242f313d26d61056129fefb415ca6fdd54d72afd
Muhammad Ramdhan <evc.adhan021@gmail.com>
2025-02-01 18:06:01 +0700

    menambahkan button Pay dan hampir finishing

--- lib/view/detail_page.dart
+++ lib/view/detail_page.dart
@@ -5,6 +5,305 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            height: MediaQuery.of(context).size.height / 3,
+            child: Container(
+              padding: const EdgeInsets.all(30.0),
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Color.fromRGBO(251, 121, 155, 1),
+                  Color.fromRGBO(251, 53, 105, 1),
+                ]),
+              ),
+              child: Transform.translate(
+                  offset: const Offset(30, -30),
+                  child: Image.asset("assets/details-page-header.png")),
+            ),
+          ),
+          Positioned(
+            top: 50,
+            left: 10,
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            height: MediaQuery.of(context).size.height / 1.4,
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Ranger Sport",
+                      style: TextStyle(
+                        color: Color.fromRGBO(97, 90, 90, 54),
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      "Ankle Men\'s Athletic Socks",
+                      style: TextStyle(
+                          color: Color.fromRGBO(97, 90, 90, 1),
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Ranger sport socks are a fusion of materials of the sturdiest quality and versatile design that suits all purpose. Each pair is made with a blend of cotton, polyester, and spandex to ensure a comfortable fit and durability.",
+                      style: TextStyle(
+                        color: Color.fromRGBO(51, 51, 51, 54),
+                        fontSize: 18,
+                        height: 1.4,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Row(
+                      children: [
+                        Container(
+                          width: 40,
+                          height: 40,
+                          padding: const EdgeInsets.all(2),
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 2, color: Colors.red),
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                          ),
+                          child: Container(
+                            width: 30,
+                            height: 30,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 40,
+                          height: 40,
+                          padding: const EdgeInsets.all(5),
+                          child: Container(
+                            width: 30,
+                            height: 30,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color.fromRGBO(251, 53, 105, 1),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 40,
+                          height: 40,
+                          padding: const EdgeInsets.all(5),
+                          child: Container(
+                            width: 30,
+                            height: 30,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color.fromRGBO(81, 223, 234, 1),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 50),
+                    const Text("More Options",
+                        style: TextStyle(
+                          color: Color.fromRGBO(97, 90, 90, 54),
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    const SizedBox(height: 20),
+                    Container(
+                      height: 80,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          AspectRatio(
+                            aspectRatio: 3.2 / 1,
+                            child: Container(
+                              padding: const EdgeInsets.all(13),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 2,
+                                    color: Colors.grey[300] ?? Colors.grey),
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 56,
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: const Color.fromRGBO(
+                                            251, 53, 105, 1)),
+                                    child: Image.asset("assets/socks-icon.png"),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  const Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        "Ankle Length Socks",
+                                        style: TextStyle(
+                                          color: Color.fromRGBO(97, 90, 90, 1),
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 2,
+                                      ),
+                                      Text("23,345",
+                                          style: TextStyle(
+                                              color: Color.fromRGBO(
+                                                  97, 90, 90, 7)))
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          AspectRatio(
+                            aspectRatio: 3.2 / 1,
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 20),
+                              padding: const EdgeInsets.all(13),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 2,
+                                    color: Colors.grey[300] ?? Colors.grey),
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 50,
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: const Color.fromRGBO(
+                                            81, 234, 234, 1)),
+                                    child: Image.asset(
+                                        "assets/socks-icon-left.png"),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  const Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        "Quarter Length Socks",
+                                        style: TextStyle(
+                                          color: Color.fromRGBO(97, 90, 90, 1),
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 2,
+                                      ),
+                                      Text("23,345",
+                                          style: TextStyle(
+                                              color: Color.fromRGBO(
+                                                  97, 90, 90, 7)))
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Container(
+                      height: 60,
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        gradient: const LinearGradient(
+                          colors: [
+                            Color.fromRGBO(251, 121, 155, 1),
+                            Color.fromRGBO(251, 53, 105, 1),
+                          ],
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey[300] ?? Colors.grey,
+                            blurRadius: 10,
+                          )
+                        ],
+                      ),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.baseline,
+                            textBaseline: TextBaseline.alphabetic,
+                            children: [
+                              Text(
+                                '\$14.',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 22,
+                                ),
+                              ),
+                              Text(
+                                '54',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "Pay",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
